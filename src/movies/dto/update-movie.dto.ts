b9a1959320cbade2e9dbf8d9e982ec4ba23a9c70
hyo <hyoseonee0729@naver.com>
2024-12-10 19:53:15 +0900

    모듈 구조 최적화

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,16 +1,17 @@
-// 수정은 일부만 가능하므로 required가 아닌 readonly로
+// CreateMovieDto 클래스를 베이스로 하는 PartialType (일부만 수정 가능하도록)
 
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { CreateMovieDto } from './create-movie.dto';
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {
   @IsString()
-  readonly title?: string;
+  readonly title: string;
 
   @IsNumber()
-  readonly year?: number;
+  readonly year: number;
 
+  @IsOptional()
   @IsString({ each: true })
-  readonly genres?: string[];
+  readonly genres: string[];
 }
